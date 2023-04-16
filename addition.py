c02b915be902b01f1f1d3475fb482e636ce87cc8
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-04-17 01:00:22 +0530

    adding two integer by taking input

---
+++ addition.py
@@ -0,0 +1,5 @@
+""" This is programm for sum of two numbers """
+a = input("Enter first digit:")
+b = input("Enter second digit :")
+c = int(a) + int(b)
+print("Addition of given digit is :" + str(c))
