7c45908a1e626d1db2fb7dde92148a6f3c3aa530
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-05-06 23:26:50 +0530

    Find factorial of number

---
+++ factorial.py
@@ -0,0 +1,13 @@
+"""Factorial of number """
+n = int(input("Enter a number :"))
+
+fact = 1
+
+if n < 0:
+    print("Factorial of number doe not exist.")
+elif n == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1, n + 1):
+        fact = fact * i
+    print("Factorial of ", n, "is", fact)
