71918100f2d0f625d53244ae2a728e41cec4f202
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-05-11 00:00:48 +0530

    find average

---
+++ AVERAGE.py
@@ -0,0 +1,11 @@
+a = int(input("Enter marks of math:"))
+b = int(input("Enter marks of Eng:"))
+c = int(input("Enter marks of sci:"))
+d = int(input("Enter marks of hist:"))
+e = int(input("Enter marks of social science :"))
+
+sum = a+b+c+d+e
+
+average = sum / 5
+
+print("Average of given marks is ", average)
