445d6570be74c572fb9fe11c0468b3a4fd84d71c
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-06-07 23:33:49 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,14 @@
+test = 254
+
+print(test, 'is', bool(test))
+
+test1 = 25.14
+
+print(test1, 'is', bool(test1))
+
+test2 = 'Python is the best'
+print(test2, 'is', bool(test2))
+
+test3 = True
+
+print(test3, 'is', bool(test3))
