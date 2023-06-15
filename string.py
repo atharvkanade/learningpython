60db3a309a5fb612b10189a95f7c5dc546f4f2c5
Atharv Kanade <113708102+atharvkanade@users.noreply.github.com>
2023-06-15 23:28:38 +0530

    Different functions of string

---
+++ string.py
@@ -0,0 +1,13 @@
+str1=“Learning python”
+
+print(str1.upper())
+
+print(str1.lower())
+
+print(str1.split())
+
+print(str1.replace(“python” , ”programming”))
+
+print(str1.count(„n‟))
+
+print(str1.find(“python”))
